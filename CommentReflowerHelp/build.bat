c96551bde4aed689d1d103b782ea357ac2af8277
Kristofel Munson <codebeast@gmail.com>
2019-02-23 10:12:17 -0500

    Add support for Visual Studio 2019
    * Bump version to 1.2.1
    * Changed chm build to look for hhc.exe in the install directory instead of %PATH%.
    * chm and gpl.rtf copy is performed by pre-build events now instead of relying on the chm build to do it.
    * Update .gitignore for VS 2019.

--- CommentReflowerHelp/build.bat
+++ CommentReflowerHelp/build.bat
@@ -1,9 +1,12 @@
-copy /Y ..\gpl.rtf ..\CommentReflower
-if errorlevel 1 exit /B 1
+rem Check for existence first due to the reversed error output.
+if not exist "%ProgramFiles(x86)%\HTML Help Workshop\hhc.exe" (
+	echo HTML Help Workshop is not installed. https://www.microsoft.com/en-us/download/details.aspx?id=21138
+	exit /B 1
+)
 
-rem hhc returns 0 on failure and 1 on success
-hhc %1.hhp
-if not errorlevel 1 exit /B 1
-
-copy /Y %1.chm ..\CommentReflower
-if errorlevel 1 exit /B 1
+rem hhc returns 0 on failure and 1 on success.
+"%ProgramFiles(x86)%\HTML Help Workshop\hhc.exe" %1.hhp
+if not errorlevel 1 (
+	echo Failed to compile help.
+	exit /B 1
+)

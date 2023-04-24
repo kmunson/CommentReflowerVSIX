3b2799a16d7780fc28f20037e11df1dad02908cb
Kristofel Munson <codebeast@gmail.com>
2023-04-24 14:31:21 -0400

    Update version to 1.3.2

--- CommentReflowerShared/CommentReflowerPackage.cs
+++ CommentReflowerShared/CommentReflowerPackage.cs
@@ -50,7 +50,7 @@ namespace CommentReflower
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     // This attribute is used to register the information needed to show this package
     // in the Help/About dialog of Visual Studio.
-    [InstalledProductRegistration("#110", "#112", "1.3.1", IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", "1.3.2", IconResourceID = 400)]
     // This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource("Menus.ctmenu", 1)]
     // Initialize the package when a text editor window is opened. This ensures that the visibility
